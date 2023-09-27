d628d6101f39014f048822cc1e6fc85777849cdd
mbravou <mbravou@unal.edu.co>
2023-09-27 00:48:35 -0500

    Makefile

--- S6C2/BravoMaria_S6C2Makefile.mk
+++ S6C2/BravoMaria_S6C2Makefile.mk
@@ -1,35 +1,25 @@
-# Archivos de entrada y salida
-DATOS_PY := makedatos.py
-DATOS_CPP := makedatos1.cpp
-GRAFICOS_PY := plotdatos.py
-GRAFICOS_CPP := plotdatos1.py
-TEX := resultados.tex
-PDF := resultados.pdf
-
 # Generar datos
 data_py:
-    python $(DATOS_PY)
+	python makedatos.py > datos.dat
 
 data_cpp:
-    g++ $(DATOS_CPP) -o makedatos1
-    ./makedatos1
+	g++ makedatos1.cpp -o makedatos1
+	./makedatos1 > datos1.dat
 
 # Gráficos
 plots_py:
-    python $(GRAFICOS_PY)
+	python plotdatos.py
 
 plots_cpp:
-    python $(GRAFICOS_CPP)
+	python plotdatos1.py
 
 # PDF
 pdf:
-    pdflatex $(TEX)
-
-# Todas las reglas
-all: data_py data_cpp plots_py plots_cpp pdf
+	pdflatex resultados.tex
 
 # Regla para limpiar archivos temporales
-#clean:
-#    rm -f makedatos1 *.pdf *.log *.aux
+clean:
+	rm -f makedatos1 *.log *.aux *.out
 
-#.PHONY: data_py data_cpp datos plots_py plots_cpp plots pdf all clean
+# Todas las reglas
+all: data_py data_cpp plots_py plots_cpp pdf clean
--- S6C2/makedatos.py
+++ S6C2/makedatos.py
@@ -6,4 +6,4 @@ t = np.linspace( 0, 10, 1000)
 y = np.cos(2 * np.pi * t)
 
 for i in range(1000):
-	print t[i],y[i]
+	print (t[i],y[i])
